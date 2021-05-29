cb7fb657b3d687261fd250d0992a428101f43d55
Steve J. South(NamJungGu) <nowage@gmail.com>
2021-05-30 01:18:22 +0900

    update 19-AutoscalingWithELB/autoscaling.tf

--- 19-AutoscalingWithELB/autoscaling.tf
+++ 19-AutoscalingWithELB/autoscaling.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "example-launchconfig" {
   instance_type        = "t2.micro"
   key_name             = aws_key_pair.mykey.key_name
   security_groups      = [aws_security_group.myinstance.id]
-  user_data            = "#!/bin/bash\napt-get update\napt-get -y install nginx\nMYIP=`ifconfig | grep 'addr:10' | awk '{ print $2 }' | cut -d ':' -f2`\necho 'this is: '$MYIP > /usr/share/nginx/html/index.html"
+  user_data            = "#!/bin/bash\napt-get update\napt-get -y install nginx net-tools\nMYIP=`ifconfig | grep 'inet '|grep 'broadcast' | awk '{ print $2 }'`\necho 'this is: '$MYIP > /var/www/html/index.html"
   lifecycle              { create_before_destroy = true }
 }
 
@@ -13,9 +13,9 @@ resource "aws_autoscaling_group" "example-autoscaling" {
   vpc_zone_identifier  = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
   launch_configuration = aws_launch_configuration.example-launchconfig.name
   min_size             = 2
-  max_size             = 2
+  max_size             = 4
   health_check_grace_period = 300
-  health_check_type = "ELB"
+  health_check_type = "EC2"
   load_balancers = [aws_elb.my-elb.name]
   force_delete = true
 
@@ -25,4 +25,3 @@ resource "aws_autoscaling_group" "example-autoscaling" {
       propagate_at_launch = true
   }
 }
-
